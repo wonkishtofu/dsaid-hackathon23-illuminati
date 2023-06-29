62231f8e814d495097601140d01b65e9ecc20f14
arushisinha98 <arushi@dsaid.gov.sg>
2023-06-29 17:12:43 +0800

    now compute hours elapsed in a separate section to use in gui workflow

--- api/demand.py
+++ api/demand.py
@@ -27,15 +27,27 @@ def get_demand_estimate(DT, DWELLING):
     time = datetime.strptime(DT[DT.find("T")+1:-1], "%H:%M:%S")
     month = date.month
     year = date.year
-    hours = time.hour
 
     # compute year to date demand and days elapsed
     ytd = 0
-    days_elapsed = 0
     for mm in range(1, month+1):
         ytd += demand[(demand.month == mm) & (demand.Region == 'Overall') & (demand.Description == 'Overall')].iloc[0]['kwh_per_acc']
+        
+    return annual, ytd
+    
+def get_hours_elapsed(DT):
+    # get current month & year
+    date = datetime.strptime(DT[:DT.find("T")], "%Y-%m-%d")
+    time = datetime.strptime(DT[DT.find("T")+1:-1], "%H:%M:%S")
+    month = date.month
+    year = date.year
+    hours = time.hour
+
+    # compute days elapsed
+    days_elapsed = 0
+    for mm in range(1, month+1):
         days_elapsed += calendar.monthrange(year, month)[1]
     
     hours_elapsed = (days_elapsed-1)*24 + hours
     
-    return annual, ytd, hours_elapsed
+    return hours_elapsed
